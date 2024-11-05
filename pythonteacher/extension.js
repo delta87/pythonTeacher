25b1b9285f139f8e5e4ddbbfe65a22adb9b97f65
delta87 <r.moosavi1381@gmail.com>
2024-11-05 22:19:35 +0330

    add src folder

--- pythonteacher/extension.js
+++ pythonteacher/extension.js
@@ -1,44 +1,6 @@
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// const vscode = require('vscode');
-
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
-
-// /**
-//  * @param {vscode.ExtensionContext} context
-//  */
-// function activate(context) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "pythonteacher" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with  registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	const disposable = vscode.commands.registerCommand('pythonteacher.helloWorld', function () {
-// 		// The code you place here will be executed every time your command is executed
-
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello World from PythonTeacher!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-// // This method is called when your extension is deactivated
-// function deactivate() {}
-
-// module.exports = {
-// 	activate,
-// 	deactivate
-// }
-
-
-
+// extension.js
 const vscode = require('vscode');
-const { exec } = require('child_process');
+const { runPythonCode } = require('./src/pythonRunner');
 
 /**
  * This function is called when the extension is activated.
@@ -68,25 +30,6 @@ function activate(context) {
     });
 
     context.subscriptions.push(disposable);
-}
-
-/**
- * Runs the selected Python code using a Python subprocess and returns the output.
- * @param {string} code
- * @param {function(string): void} callback - A callback function to handle the output.
- */
-function runPythonCode(code, callback) {
-    // Save the Python code to a temporary file
-    const pythonCommand = `python -c "${code.replace(/"/g, '\\"')}"`;
-
-    exec(pythonCommand, (error, stdout, stderr) => {
-        if (error) {
-            callback(`Error: ${stderr}`);
-        } else {
-			console.log(stdout);
-            callback(`Output: ${stdout}`);
-        }
-    });
 }
 
 /**

a1eb91bb4726d6b3037a42fddbbe88d110e74f19
delta87 <r.moosavi1381@gmail.com>
2024-11-08 14:12:51 +0330

    add side Bar files

--- pythonteacher/extension.js
+++ pythonteacher/extension.js
@@ -1,40 +1,14 @@
-// extension.js
 const vscode = require('vscode');
-const { runPythonCode } = require('./src/pythonRunner');
+const SideBarViewProvider = require('./src/sideBar');
 
-/**
- * This function is called when the extension is activated.
- * @param {vscode.ExtensionContext} context
- */
+
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('pythonteacher.checkTask', async () => {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) {
-            vscode.window.showErrorMessage('No editor is active');
-            return;
-        }
-
-        const selectedText = editor.document.getText(editor.selection);
-        if (!selectedText) {
-            vscode.window.showErrorMessage('Please select some code to check');
-            return;
-        }
-
-        try {
-            runPythonCode(selectedText, (output) => {
-                vscode.window.showInformationMessage(output);
-            });
-        } catch (error) {
-            vscode.window.showErrorMessage('Error executing Python code: ' + error.message);
-        }
-    });
-
-    context.subscriptions.push(disposable);
+    // Register the Webview view
+    context.subscriptions.push(
+        vscode.window.registerWebviewViewProvider('pythonTeacherSidebar', new SideBarViewProvider(context))
+    );
 }
 
-/**
- * This function is called when the extension is deactivated.
- */
 function deactivate() {}
 
 module.exports = {
---
+++ pythonteacher/src/sideBar.js
@@ -0,0 +1,43 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+class SideBarViewProvider {
+    constructor(context) {
+        this.context = context;
+    }
+
+    resolveWebviewView(webviewView) {
+        webviewView.webview.options = {
+            enableScripts: true
+        };
+
+        try {
+            // Attempt to load the HTML content from the external file
+            webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+        } catch (error) {
+            console.error("Error loading sidebar HTML:", error);
+            webviewView.webview.html = "<html><body><h2>Error loading sidebar content</h2></body></html>";
+        }
+
+        webviewView.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'openLesson':
+                    vscode.commands.executeCommand(message.command);
+                    break;
+            }
+        });
+    }
+
+    getHtmlForWebview(webview) {
+        const htmlPath = path.join(this.context.extensionPath, 'src/pages/sideBar.html');
+        
+        if (!fs.existsSync(htmlPath)) {
+            throw new Error(`HTML file not found at path: ${htmlPath}`);
+        }
+
+        return fs.readFileSync(htmlPath, 'utf8');
+    }
+}
+
+module.exports = SideBarViewProvider;

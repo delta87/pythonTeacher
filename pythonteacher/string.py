82173ae7dd3acbb5213980aff6e1078b027e6c2f
Hassan Rad <radhasssan@gmail.com>
2024-11-12 15:16:25 +0330

    The problems were defined

---
+++ pythonteacher/string.py
@@ -0,0 +1,11 @@
+# تمام حروف، ارقام و نمادهای خاص را از یک رشته معین بشمارید
+
+def find_digits_chars_symbols(sample_str):
+    char = 0
+    digit = 0
+    symbol = 0
+
+    return char, digit, symbol
+
+sample_str = "P@yn2at&#i5ve"
+find_digits_chars_symbols(sample_str)
